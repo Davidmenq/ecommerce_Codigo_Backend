d0cf620dea4024d9c9f7735ed3dc5b3d52b35bca
Stefano Zevallos Reyes <szevallosnl@gmail.com>
2023-09-01 18:34:03 -0500

    CRUD completo categorias , subir imagenes fucionando , funcionando usuario controller

--- app.py
+++ app.py
@@ -7,15 +7,12 @@ from os import environ
 from dotenv import load_dotenv
 from models import *
 from flasgger import Swagger
-# from controllers import (CategoriasController, 
-#                          RegistroController, 
-#                          LoginController, 
-#                          SubirImagenController, 
-#                          DevolverImagenController,
-#                          ProductosController,
-#                          PedidosController,
-#                          UsuarioController,
-#                          CambiarPasswordController)
+from controllers import (RegistroController , 
+                         CategoriasController , 
+                         ProductosController, 
+                         SubirImagenController,
+                         DevolverImagenController,CategoriaController)
+
 from flask_jwt_extended import JWTManager
 # convierte un string en formato json a un diccionario
 from json import load
@@ -23,21 +20,21 @@ from datetime import timedelta
 
 # sirve para cargar mis variables declaradas en el archivo .env como si fueran variables de entorno
 load_dotenv()
-# swaggerData = load(open('swagger_data.json', 'r'))
+swaggerData = load(open('swagger_data.json', 'r'))
 
 # https://github.com/flasgger/flasgger#customize-default-configurations
-# swaggerConfig = {
-#     'headers': [], # las cabeceras que van a aceptar nuestra documentacion
-#     'specs': [
-#         {
-#             'endpoint': '', # el endpoint inicial de nuestra documentacion 
-#             'route': '/'
-#         }
-#     ],
-#     'static_url_path': '/flasgger_static', # cargar los archivos staticos que serian el css y js de la libreria
-#     # 'swagger_ui': True, # indicar si queremos cargar la interfaz grafica o no
-#     'specs_route' : '/documentacion' # el endpoint en el cual ahora se ingresara a mi swagger
-# }
+swaggerConfig = {
+    'headers': [], # las cabeceras que van a aceptar nuestra documentacion
+    'specs': [
+        {
+            'endpoint': '', # el endpoint inicial de nuestra documentacion 
+            'route': '/'
+        }
+    ],
+    'static_url_path': '/flasgger_static', # cargar los archivos staticos que serian el css y js de la libreria
+    # 'swagger_ui': True, # indicar si queremos cargar la interfaz grafica o no
+    'specs_route' : '/documentacion' # el endpoint en el cual ahora se ingresara a mi swagger
+}
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=environ.get('DATABASE_URL')
@@ -49,9 +46,9 @@ app.config['SQLALCHEMY_DATABASE_URI']=environ.get('DATABASE_URL')
 
 # JWTManager(app)
 
-# Swagger(app, template=swaggerData, config=swaggerConfig)
+Swagger(app, template=swaggerData, config=swaggerConfig)
 # CORS > Cross Origin Resource Sharing (sirve para indicar quien puede tener acceso a mi API, indicando el dominio (origins), las cabeceras (allow_headers), y los metodos (methods))
-# CORS(app, origins='*')
+CORS(app, origins='*')
 api = Api(app)
 
 conexion.init_app(app)
@@ -59,12 +56,13 @@ conexion.init_app(app)
 Migrate(app, conexion)
 
 # rutas
-# api.add_resource(CategoriasController, '/categorias')
-# api.add_resource(RegistroController, '/registro')
+api.add_resource(CategoriasController, '/categorias')
+api.add_resource(CategoriaController, '/categoria/<int:id>')
+api.add_resource(RegistroController, '/registro')
 # api.add_resource(LoginController, '/login')
-# api.add_resource(SubirImagenController, '/subir-imagen')
-# api.add_resource(DevolverImagenController, '/imagenes/<nombreImagen>')
-# api.add_resource(ProductosController, '/productos')
+api.add_resource(SubirImagenController, '/subir-imagen')
+api.add_resource(DevolverImagenController, '/imagenes/<nombreImagen>')
+api.add_resource(ProductosController, '/productos')
 # api.add_resource(PedidosController, '/pedidos')
 # api.add_resource(UsuarioController, '/perfil')
 # api.add_resource(CambiarPasswordController, '/cambiar-password')
---
+++ controllers/categoria.py
@@ -0,0 +1,135 @@
+from flask_restful import Resource, request
+from models import CategoriaModel 
+from dtos import CategoriaRequestDto 
+from utilitarios import conexion
+# from decorators import validador_usuario_admin
+# get_jwt_identity > devolvera la identificacion del usuario de la token
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+class CategoriasController(Resource):
+
+    # @validador_usuario_admin
+    def post(self):
+        """
+        Creacion de una categoria
+        ---
+        operationId: post_categoria
+        description: Creacion de una nueva categoria con la imagen
+        tags:
+            - Categoria
+        parameters:
+            - in: body
+              name: body
+              schema:
+                properties:
+                    nombre:
+                        type: string
+                        example: 'Lorem'
+                    imagen:
+                        type: string
+                        example: 'https://www.google.com'
+        security:
+            -   Bearer: []
+        responses:
+            201:
+              description: Categoria creada exitosamente
+              schema:
+                 $ref: '#/definitions/Categoria'
+        """
+        dto = CategoriaRequestDto()
+        # identificador = get_jwt_identity()
+        # print(identificador)
+        
+        try:
+            dataVerificada = dto.load(request.get_json())
+            nuevaCategoria = CategoriaModel(**dataVerificada)
+            conexion.session.add(nuevaCategoria)
+            conexion.session.commit()
+            return {
+                'message': 'Categoria creada exitosamente',
+                'content': ''
+            }, 201
+        
+        except Exception as e:
+            # rollback > retroceder el estado antes de que se de el error en la base de datos
+            conexion.session.rollback()
+            return{
+                'message': 'Error al crear la categoria',
+                'content': e.args
+            }, 400
+
+    def get(self):
+        """
+        file: getCategoria.yml
+        """
+        categorias = conexion.session.query(CategoriaModel).all()
+        dto = CategoriaRequestDto()
+        resultado =dto.dump(categorias, many=True)
+
+        return {
+            'content': resultado
+        },200
+    
+class CategoriaController(Resource):    
+    def get(self,id):
+        categoriaEncontrada = conexion.session.query(CategoriaModel).filter_by(id=id).first()
+        if not categoriaEncontrada:
+            return {
+                 'message': 'El usuario no existe'
+             }, 404
+        dto = CategoriaRequestDto() 
+        categoria = dto.dump(categoriaEncontrada)  
+
+        return categoria
+    
+    def put(self,id):
+        categoriaEncontrada = conexion.session.query(CategoriaModel).filter_by(id=id).first()
+        if not categoriaEncontrada:
+            return {
+                 'message': 'El usuario no existe'
+             }, 404
+        data = request.get_json()
+
+        dto = CategoriaRequestDto() 
+        try:
+            dataValidada = dto.load(data)
+            # UPDATE usuarios SET nombre='...', apellido='...' ... WHERE id = '...';
+            usuarioActualizados = conexion.session.query(CategoriaModel).filter_by(id=id).update(dataValidada)
+
+            print(usuarioActualizados)
+
+            conexion.session.commit()
+
+            return {
+                'message': 'Categoria actualizada exitosamente'
+            }
+        
+        except Exception as error:
+            return {
+                'message': 'Error al actualizar la categoria',
+                'content': error.args
+            }  
+    def delete(self,id):
+        categoriaEncontrada = conexion.session.query(CategoriaModel).filter_by(id=id).first()
+        if not categoriaEncontrada:
+            return {
+                 'message': 'El usuario no existe'
+             }, 404
+
+        try:
+            categoriaEncontrada = conexion.session.query(CategoriaModel).filter_by(id=id).delete()
+            print(categoriaEncontrada)
+
+            conexion.session.commit()
+
+            return {
+                'message': 'Categoria eliminada exitosamente'
+            }
+        
+        except Exception as error:
+            return {
+                'message': 'Error al eliminar la categoria',
+                'content': error.args
+            } 
+          
